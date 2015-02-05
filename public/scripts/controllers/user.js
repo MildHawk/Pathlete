404520b6db3418a3d42df272163eb2a360e2a481
Jackson Hoose <jacksonhoose@gmail.com>
2015-02-04 16:32:39 -0800

    made the challenge section look a bit prettier

--- public/scripts/controllers/user.js
+++ public/scripts/controllers/user.js
@@ -1,5 +1,17 @@
 function UserController(user) {
   this.user = user;
+  // calculate percent completed of goal
+  if (this.user.challenge.goal && this.user.challenge.raised) {
+    /**
+     * converts amount raised into percent string
+     * and adds to user challenge object.
+     */
+    var goal = parseInt(this.user.challenge.goal.substr(1), 10);
+    var raised = parseInt(this.user.challenge.raised.substr(1), 10);
+    var completed = (raised/goal) + '';
+    this.user.challenge.completed = completed.split('.')[1].substr(0, 2) + '%';
+  }
+
 }
 UserController.$inject = ['user'];
 
