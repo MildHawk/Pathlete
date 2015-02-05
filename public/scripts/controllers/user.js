d6c9c4807e2c4876d500ce3ba91a9574b7a9d7fc
Jackson Hoose <jacksonhoose@gmail.com>
2015-02-04 16:57:14 -0800

    finished challenge section more and stubbed variables

--- public/scripts/controllers/user.js
+++ public/scripts/controllers/user.js
@@ -1,16 +1,25 @@
 function UserController(user) {
   this.user = user;
+  
   // calculate percent completed of goal
   if (this.user.challenge.goal && this.user.challenge.raised) {
+    this.user.challenge.completed = getGoalPercent(this.user.challenge.goal, this.user.challenge.raised);
+  }
+
+  function getGoalPercent(goal, raised) {
     /**
      * converts amount raised into percent string
      * and adds to user challenge object.
      */
-    var goal = parseInt(this.user.challenge.goal.substr(1), 10);
-    var raised = parseInt(this.user.challenge.raised.substr(1), 10);
+    goal = parseInt(goal.substr(1), 10);
+    raised = parseInt(raised.substr(1), 10);
     var completed = (raised/goal) + '';
-    this.user.challenge.completed = completed.split('.')[1].substr(0, 2) + '%';
+    return completed.split('.')[1].substr(0, 2) + '%';
   }
+
+  this.donate = function() {
+    // trigger donate modal
+  };
 
 }
 UserController.$inject = ['user'];
